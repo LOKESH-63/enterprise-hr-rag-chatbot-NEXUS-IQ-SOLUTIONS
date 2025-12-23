eb846bfef5c3d6b50c205a3ddb162585b761df62
D lokesh reddy <dlokeshreddy3074@gmail.com>
2025-12-23 12:47:40 +0530

    Integrate company logo into app layout
    Added company logo to the HR Policy Chatbot interface.

--- app.py
+++ app.py
@@ -13,6 +13,17 @@ st.set_page_config(page_title="HR Policy Chatbot", page_icon="🏢")
 
 PDF_FILE = "Sample_HR_Policy_Document.pdf"
 
+from PIL import Image
+
+# -------- COMPANY LOGO --------
+logo = Image.open("nexus_iq_logo.png")
+
+col1, col2 = st.columns([1, 6])
+with col1:
+    st.image(logo, width=70)   # small logo
+with col2:
+    st.markdown("## **Enterprise RAG-based HR Chatbot**")
+
 # ---------------- LOGIN SYSTEM ----------------
 USERS = {
     "employee": {"password": "employee123", "role": "Employee"},
@@ -47,7 +58,7 @@ if not st.session_state.logged_in:
     st.stop()
 
 # ---------------- HEADER ----------------
-st.title("🏢 NEXUS IQ SOLUTIONS HR Policy Chatbot")
+st.title(" NEXUS IQ SOLUTIONS ")
 st.caption(f"Logged in as: **{st.session_state.role}**")
 
 if st.button("Logout"):
